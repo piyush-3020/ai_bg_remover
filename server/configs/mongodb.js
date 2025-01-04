3229a1ec96b6614a785dc0a76c7b00d88ca6c362
piyush-3020 <piyush30rathwe@gmail.com>
2025-01-04 17:55:57 +0530

    database changes

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
   mongoose.connection.on("connected", () => {
     console.log("Database Connected");
   });
-  await mongoose.connect(`${process.env.MONGODB_URI}/bg-removal`);
+  await mongoose.connect(process.env.MONGODB_URI);
 };
 
 export default connectDB;
